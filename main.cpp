ff74216ae6b9ff6b9d47b839dbb7d65a6cd04898
samson2 <nobindog1@gmail.com>
2014-10-07 13:02:31 -0500

    Update main.cpp
    instead of y0-y10 it's yn5-y5; allows for a more expandable program with less hassle.

--- main.cpp
+++ main.cpp
@@ -13,30 +13,31 @@ int main()
     cout << "What is the y intercept (b)?\n"; //y intercept
         short b;
         cin >> b;
-    short y0= slope*-5+b; //will probably expand to -10-10 when I get around to it.
-    short y1= slope*-4+b; //Just solving where the points go
-    short y2= slope*-3+b;
-    short y3= slope*-2+b;
-    short y4= slope*-1+b; //I feel like this could be simplified with a while loop
-    short y5= slope*0+b;
-    short y6= slope*1+b;
-    short y7= slope*2+b;
-    short y8= slope*3+b;
-    short y9= slope*4+b;
-    short y10= slope*5+b;
+    short yn5= slope*-5+b; //will probably expand to -10 to 10 when I get around to it.
+    short yn4= slope*-4+b; //Just solving where the points go
+    short yn3= slope*-3+b;
+    short yn2= slope*-2+b;
+    short yn1= slope*-1+b; //I feel like this could be simplified with a while loop
+    short y0= slope*0+b;
+    short y1= slope*1+b;
+    short y2= slope*2+b;
+    short y3= slope*3+b;
+    short y4= slope*4+b;
+    short y5= slope*5+b;
     
     cout << " x y \n" //muh C style programming of some sort of table
          << "=====\n"; 
-    cout << "-5, " << y0 << "\n";
-    cout << "-4, " << y1 << "\n";
-    cout << "-3, " << y2 << "\n"; //TOO MANY COUT's!! Short hand wasn't cooperating
-    cout << "-2, " << y3 << "\n"; //correctly
-    cout << "-1, " << y4 << "\n";
-    cout << "0, " << y5 << "\n";
-    cout << "1, " << y6 <<"\n";
-    cout << "2, " << y7 <<"\n";
-    cout << "3, " << y8 << "\n";
-    cout << "4, " << y9 << "\n";
-    cout << "5, " << y10 << "\n";
+    cout << "-5, " << yn5 << "\n";
+    cout << "-4, " << yn4 << "\n";
+    cout << "-3, " << yn3 << "\n"; //TOO MANY COUT's!! Short hand wasn't cooperating
+    cout << "-2, " << yn2 << "\n"; //correctly
+    cout << "-1, " << yn1 << "\n";
+    cout << "0, " << y0 << "\n";
+    cout << "1, " << y1 <<"\n";
+    cout << "2, " << y2 <<"\n";
+    cout << "3, " << y3 << "\n";
+    cout << "4, " << y4 << "\n";
+    cout << "5, " << y5 << "\n";
     
 }
+
